931be3085d3650c10808f241a264e05ee18f9bc2
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-12-08 18:55:44 +0300

    TinyMCE integration for yz admin

--- TinyMCE.php
+++ TinyMCE.php
@@ -2,11 +2,21 @@
 
 namespace yz\admin\tinymce;
 
+use yii\helpers\ArrayHelper;
+
 
 /**
  * Class TinyMCE
  */
-class TinyMCE
+class TinyMCE extends \pendalf89\tinymce\TinyMce
 {
+    public function run()
+    {
+        $this->clientOptions = ArrayHelper::merge([
+            'language' => \Yii::$app->language,
+        ], $this->clientOptions);
+
+        return parent::run();
+    }
 
 } 
