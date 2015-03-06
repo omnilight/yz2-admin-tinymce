a1a91fc8189ef6dca0396793baf8df8238f33b2d
Pavel <pavel.agalecky@gmail.com>
2015-03-06 13:59:08 +0300

    Fix

--- TinyMCE.php
+++ TinyMCE.php
@@ -13,7 +13,6 @@ class TinyMCE extends \pendalf89\tinymce\TinyMce
     public function run()
     {
         $bundle = AdminTinyMceAsset::register($this->view);
-        AdminTinyMceAsset::
         $this->clientOptions = ArrayHelper::merge([
             'language' => \Yii::$app->language,
             'content_css' => $bundle->baseUrl.'/content.css',
